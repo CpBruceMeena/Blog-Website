8b476bd36ed86103cefe81a81abb66426f8c0b9c
CpBruceMeena <sandeep10198@gmail.com>
2019-11-08 21:55:29 +0530

    debugging time

--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -57,34 +57,34 @@ class DraftListView(LoginRequiredMixin, ListView):
 
 @login_required
 def post_publish(request, pk):
-    post = get_object_or_404(Post, pk = pk)
+    post = get_object_or_404(Post, pk=pk)
     post.publish
-    return redirect('post_detail', pk = pk)
+    return redirect('post_detail', pk=pk)
 
 
 @login_required
 def add_comment_to_post(request, pk):
-    post = get_object_or_404(Post, pk = pk)
+    post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
         form = CommentForm(request.POST)
         if form.is_valid():
             comment = form.save(commit= False)
             comment.post = post
             comment.save()
-            return redirect('post_detail', pk = post.pk)
+            return redirect('post_detail', pk=post.pk)
     else:
         form = CommentForm()
     return render(request, 'blog/comment_form.html', {'form':form})
 
 @login_required
 def comment_approve(request, pk):
-    comment = get_object_or_404(Comment,pk = pk)
+    comment = get_object_or_404(Comment,pk=pk)
     comment.approve()
-    return redirect('post_detail', pk = comment.post.pk)
+    return redirect('post_detail', pk=comment.post.pk)
 
 @login_required
 def comment_remove(request, pk):
-    comment = get_object_or_404(Comment, pk = pk)
+    comment = get_object_or_404(Comment, pk=pk)
     post_pk = comment.post.pk
     comment.delete()
-    return redirect('post_detail', pk = post_pk)
+    return redirect('post_detail', pk=post_pk)
--- myblog/myblog/urls.py
+++ myblog/myblog/urls.py
@@ -17,10 +17,11 @@ from django.contrib import admin
 from django.conf.urls import url, include
 from django.contrib.auth import views
 
+
+
 urlpatterns = [
     url('admin/', admin.site.urls),
     url(r'', include("blog.urls")),
-    url(r'accounts/login/$', views.login, name = 'login'),
-    url(r'accounts/logout/$', views.logout, name = 'logout', kwargs = {'next_page':"/"}),
-
+    url(r'accounts/login/$', views.LoginView.as_view(), name = 'login'),
+    url(r'accounts/logout/$', views.LogoutView.as_view(), name = 'logout', kwargs = {'next_page':"/"}),
 ]
